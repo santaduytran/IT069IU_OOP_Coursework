b4553845fcffcb4b6d2e9c83601bde0ca1d47a52
Trần Nguyễn Khánh Duy <71444587+santaduytran@users.noreply.github.com>
2022-05-21 21:51:33 +0700

    Add files via upload

---
+++ Lab 3/src/question3/Person.java
@@ -0,0 +1,27 @@
+package question3;
+
+public class Person {
+  private String name;
+  private String address;
+
+  public Person(String name, String address) {
+    this.name = name;
+    this.address = address;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
+  public String toString() {
+    return String.format("Person[name=%s, address=%s]", name, address);
+  }
+}
