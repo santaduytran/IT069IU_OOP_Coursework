fda2ec063a9a39aea17148da81edc3eb0a35a3d6
Trần Nguyễn Khánh Duy <71444587+santaduytran@users.noreply.github.com>
2022-05-21 21:52:18 +0700

    Create Main.java

---
+++ Lab 3/src/question4/Main.java
@@ -0,0 +1,10 @@
+package question4;
+
+public class Main {
+  public static void main(String[] args) {
+    Point2D p2d = new Point2D(13, 28);
+    Point3D p3d = new Point3D(6, 5, 10);
+    System.out.println(p2d.toString());
+    System.out.println(p3d.toString());
+  }
+}
