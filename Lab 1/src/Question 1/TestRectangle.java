9624c0be0819b7eaf81148d85596687987d6a51a
Trần Nguyễn Khánh Duy <71444587+santaduytran@users.noreply.github.com>
2022-05-21 21:31:01 +0700

    Add files via upload

---
+++ Lab 1/src/Question 1/TestRectangle.java
@@ -0,0 +1,19 @@
+package Question1;
+
+import Question1.Rectangle;
+
+public class TestRectangle {
+
+    public static void main(String[] args) {
+        Rectangle r1 = new Rectangle(6, 10);
+        r1.visualize();
+
+        Rectangle r2 = new Rectangle(-6, 10);
+        r2.visualize();
+
+        Rectangle r3 = new Rectangle(6, -10);
+        r3.visualize();
+
+    }
+
+}
